b2c765e18465f15f47b1ab826db9a8e80ad491b5
Daniel Gomm <daniel.gomm@cwi.nl>
2025-10-06 18:08:35 +0200

    improve prompt model

--- common/model.py
+++ common/model.py
@@ -15,14 +15,19 @@ class Prompt(BaseModel):
     prompt_template: PromptTemplate
     common_request: ResponsesRequest
 
-    def to_file(self, path: PathLike) -> None:
+    def save(self, path: PathLike) -> None:
+        if not str(path).endswith(".json"):
+            raise ValueError("Prompt has to be saves as \".json\" file.")
         Path(path).parent.mkdir(parents=True, exist_ok=True)
 
         with open(path, 'w+') as f:
             f.write(self.model_dump_json(indent=4))
 
     @classmethod
-    def from_file(cls, path: PathLike) -> Self:
+    def load(cls, path: PathLike) -> Self:
         with open(path, 'r') as f:
             return Prompt.model_validate_json(f.read())
+
+    def __str__(self):
+        return f"Name: {self.name}\nDescription: {self.description}\n\nMessages:\n{"\n".join([f"Role: {m.role}\nContent:\n{m.content}" for m in self.prompt_template.messages])}"
 
